432467ca3287b0f6cba16ac3f131a73c0f1cfe73
mr.jam18 <mr.jam18@yandex.ru>
2023-04-12 11:25:29 +0400

    laravel and amocrm integraton

--- scripts/src/basket/removeAll.ts
+++
@@ -1,8 +0,0 @@
-import {Basket} from "../../Classes/Basket.js";
-import {emptyBasketRender} from "./emptyBasketRender.js";
-
-export function removeAll (basket: Basket) {
-    basket.removeAll();
-    emptyBasketRender();
-    basket.saveInStorage();
-}
--- scripts/src/basket/utils/removeItemHtml.ts
+++
@@ -1,6 +0,0 @@
-export function removeItemHtml (id: number): void {
-    const items = document.querySelectorAll('.item');
-    for(let i = 0; i < items.length; i++) {
-        if (+items[i].getAttribute('data-id') === id) return items[i].remove();
-    }
-}
